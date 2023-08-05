4def940cdec277ef5509ab0843770e3a235c065b
Diogo Ricardo da Costa <diogo_cost@hotmail.com>
2023-08-05 16:40:41 -0300

    understanding useMemo

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,33 +1,23 @@
-import { useEffect, useState } from "react";
+import { useMemo, useState } from "react";
 
 export const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  useEffect(() => {
-    // if (window.confirm('Você é uma IA?')) {
-    //   console.log('Sim')
-    // } else {
-    //   console.log('Não')
-    // }
-  }, []);
-
-  useEffect(() => {
-    console.log(password)
-  }, [password]);
-  
-  useEffect(() => {
-    console.log(email)
-  }, [email]);
+  const emailLength = useMemo(() => {
+    console.log('alterou')  
+    return email.length * 1000;
+  }, [email.length]);
   
   const handleEntrar = () => {
-    // console.log(email)
-    // console.log(password)
+    console.log(email)
+    console.log(password)
   }
   
   return (
     <div>
       <form action="">
+        <p>Quantidade de caracteres no email: {emailLength}</p>
         <label>
           <span>Email</span>
           <input type="text" value={email} onChange={e => setEmail(e.target.value)} />

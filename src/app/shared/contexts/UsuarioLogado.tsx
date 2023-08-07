f76bf89bd0df964712186adcbef8aa33f0406040
Diogo Ricardo da Costa <diogo_cost@hotmail.com>
2023-08-07 17:36:13 -0300

    passing state in the context

--- src/app/shared/contexts/UsuarioLogado.tsx
+++ src/app/shared/contexts/UsuarioLogado.tsx
@@ -1,4 +1,4 @@
-import { createContext, useCallback } from "react";
+import { createContext, useCallback, useEffect, useState } from "react";
 
 interface IUsuarioLogadoContextData {
   nomeDoUsuario: string;
@@ -11,12 +11,21 @@ interface IUsuarioLogadoProviderProps {
   children: React.ReactNode;
 }
 export const UsuarioLogadoProvider: React.FC<IUsuarioLogadoProviderProps> = ({ children }) => {
+  const [nome, setNome] = useState('');
+
+  useEffect(() => {
+    setTimeout(() => {
+      setNome('Lucas');
+    }, 1000);
+  });
+
+  
   const handleLogout = useCallback(() => {
     console.log('Logout executou');
   }, [])
   
   return (
-    <UsuarioLogadoContext.Provider value={{ nomeDoUsuario: 'Banana', logout: handleLogout }}>
+    <UsuarioLogadoContext.Provider value={{ nomeDoUsuario: nome, logout: handleLogout }}>
       { children }
     </UsuarioLogadoContext.Provider>
   );
